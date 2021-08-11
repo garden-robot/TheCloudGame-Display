a86132ab6dcbbea5778ab4bfd862db89b8927207
jude <kap9566@nyu.edu>
2021-08-11 17:12:55 -0400

    Finished first pass pf Win State

--- Clouds-Win-Display/Clouds-Win-Display.ino
+++ Clouds-Win-Display/Clouds-Win-Display.ino
@@ -1,3 +1,16 @@
+/*
+   Shared Game Mode Across All Blinks
+   This can work for as many different game modes as you need
+   Switching between them simply requires a call to changeMode()
+   The signalState takes care of making sure all other Blinks change too
+*/
+
+enum signalStates {INERT, GO, RESOLVE};
+byte signalState = INERT;
+
+enum gameModes {MODE1, MODE2, MODE3};//these modes will simply be different colors
+byte gameMode = MODE1;//the default mode when the game begins
+
 //time values for delta
 uint32_t timeOfSend = 0;
 uint32_t timeOfPress = 0; // use this to show the button was pressed in connect mode
@@ -29,32 +42,167 @@ word backgroundTime;
 
 #define FADE_TO_WIN 500
 
-Timer fadeToWin;
-#define FADE_TO_WIN_DELAY 4000
+Timer fadeToBright;
+#define FADE_TO_BRIGHT_DELAY 2000
+
+Timer fadeToCloud;
+#define FADE_TO_CLOUD_DELAY 3000
+
+Timer sunSpotFade;
+#define SUN_SPOT_FADE 1000
+
+Timer fadeToDark;
+#define FADE_TO_DARK_DELAY 1000
 
 enum modeStates {LOSE, WIN};
 byte currentMode;
-bool winStates[6] = {false, false, false, false, false, false};
-void setup() {
-  // put your setup code here, to run once:
-  randomize();
-  fadeToWin.set(FADE_TO_WIN_DELAY);
 
+
+void setup() {
+  randomize();
 }
 
 void loop() {
-  fadeToDark();
-  if (fadeToWin.isExpired()) {
 
-    silverLiningDisplay();
+  // The following listens for and updates game state across all Blinks
+  switch (signalState) {
+    case INERT:
+      inertLoop();
+      break;
+    case GO:
+      goLoop();
+      break;
+    case RESOLVE:
+      resolveLoop();
+      break;
   }
 
+  // The following is loops for each of our game states
+  switch (gameMode) {
+    case MODE1:
+      mode1Loop();
+      break;
+    case MODE2:
+      mode2Loop();
+      break;
+    case MODE3:
+      mode3Loop();
+      break;
+  }
+
+  // communicate with neighbors
+  // share both signalState (i.e. when to change) and the game mode
+  byte sendData = (signalState << 2) + (gameMode);
+  setValueSentOnAllFaces(sendData);
+}
+
+/*
+   Mode 1
+*/
+void mode1Loop() {
+
+  if (buttonSingleClicked()) {
+    changeMode(MODE2);
+  }
+
+  setColor(WHITE);
+}
+
+/*
+   Mode 2
+*/
+void mode2Loop() {
+
+  if (buttonSingleClicked()) {
+    changeMode(MODE1);
+  }
+
+  setColor(WHITE);
+  fadeToNoLight();
+  if (fadeToDark.isExpired()) {
+    silverLiningDisplay();
+  }
+}
+
+/*
+   Mode 3
+*/
+void mode3Loop() {
+
+  if (buttonSingleClicked()) {
+    changeMode(MODE1);
+  }
+
+  setColor(BLUE);
 }
 
 
+/*
+   pass this a game mode to switch to
+*/
+void changeMode( byte mode ) {
+  if (mode == MODE2) {
+    setupFadeTimer.set(backgroundTime + SETUP_FADE_UP_INTERVAL + random(SETUP_FADE_DELAY));
+    fadeToBright.set(FADE_TO_BRIGHT_DELAY);
+    fadeToDark.set(FADE_TO_DARK_DELAY);
+    fadeToCloud.set(FADE_TO_CLOUD_DELAY);
+  }
+  gameMode = mode;  // change my own mode
+  signalState = GO; // signal my neighbors
+}
 
-void fadeToDark() {
-  setColor(WHITE);
+
+/*
+   This loop looks for a GO signalState
+   Also gets the new gameMode
+*/
+void inertLoop() {
+
+  //listen for neighbors in GO
+  FOREACH_FACE(f) {
+    if (!isValueReceivedOnFaceExpired(f)) {//a neighbor!
+      if (getSignalState(getLastValueReceivedOnFace(f)) == GO) {//a neighbor saying GO!
+        signalState = GO;
+        changeMode(getGameMode(getLastValueReceivedOnFace(f)));
+      }
+    }
+  }
+}
+
+/*
+   If all of my neighbors are in GO or RESOLVE, then I can RESOLVE
+*/
+void goLoop() {
+  signalState = RESOLVE;//I default to this at the start of the loop. Only if I see a problem does this not happen
+
+  //look for neighbors who have not heard the GO news
+  FOREACH_FACE(f) {
+    if (!isValueReceivedOnFaceExpired(f)) {//a neighbor!
+      if (getSignalState(getLastValueReceivedOnFace(f)) == INERT) {//This neighbor doesn't know it's GO time. Stay in GO
+        signalState = GO;
+      }
+    }
+  }
+}
+
+/*
+   This loop returns me to inert once everyone around me has RESOLVED
+   Now receive the game mode
+*/
+void resolveLoop() {
+  signalState = INERT;//I default to this at the start of the loop. Only if I see a problem does this not happen
+
+  //look for neighbors who have not moved to RESOLVE
+  FOREACH_FACE(f) {
+    if (!isValueReceivedOnFaceExpired(f)) {//a neighbor!
+      if (getSignalState(getLastValueReceivedOnFace(f)) == GO) {//This neighbor isn't in RESOLVE. Stay in RESOLVE
+        signalState = RESOLVE;
+      }
+    }
+  }
+}
+
+void fadeToLight() {
 
   uint32_t delta = millis() - timeOfSend;
 
@@ -66,15 +214,34 @@ void fadeToDark() {
     byte midline = 185;
     byte rate = 6;
     byte brightness = midline + (amplitude * sin8_C ( (phaseShift + millis() / rate) % 255)) / 255;
-    byte saturation = map(delta, 0, FADE_TO_WIN_DELAY, 0, 255);
+    byte saturation = 255 -  map(fadeToBright.getRemaining(), 0, FADE_TO_BRIGHT_DELAY, 0, 255);
     Color faceColor = makeColorHSB(0, 0, saturation);
     setColorOnFace(faceColor, f);
   }
 }
 
 
-void silverLiningDisplay() {
+void fadeToNoLight() {
 
+  uint32_t delta = millis() - timeOfSend;
+
+
+  FOREACH_FACE(f) {
+    // minimum of 125, maximum of 255
+    byte phaseShift = 60 * f;
+    byte amplitude = 55;
+    byte midline = 185;
+    byte rate = 6;
+    byte brightness = midline + (amplitude * sin8_C ( (phaseShift + millis() / rate) % 255)) / 255;
+    byte saturation = map(fadeToDark.getRemaining(), 0, FADE_TO_DARK_DELAY, 0, 255);
+    Color faceColor = makeColorHSB(0, 0, saturation);
+    setColorOnFace(faceColor, f);
+  }
+}
+
+void silverLiningDisplay() {
+  Color faceColor_lining;
+  Color faceColor_cloud;
   uint32_t delta = millis() - timeOfSend;
 
 
@@ -89,9 +256,20 @@ void silverLiningDisplay() {
     byte midline = 185;
     byte rate = 10;
     byte brightness = midline + (amplitude * sin8_C ( (phaseShift + millis() / rate) % 255)) / 255;
-    byte saturation = map(delta, 0, 300, 0, 255);
-    Color faceColor_lining = makeColorHSB(0, 0, brightness);
-    Color faceColor_cloud = makeColorHSB(160, saturation, 255);
+    byte saturation = 255 -  map(fadeToBright.getRemaining(), 0, FADE_TO_BRIGHT_DELAY, 0, 255);
+     byte cloudSaturation = 255 -  map(fadeToCloud.getRemaining(), 0, FADE_TO_CLOUD_DELAY, 0, 255);
+    if (!fadeToCloud.isExpired()) {
+
+       faceColor_lining = makeColorHSB(0, 0, saturation);
+      faceColor_cloud = makeColorHSB(160, saturation, cloudSaturation);
+
+    }
+
+    if (fadeToBright.isExpired()) {
+      faceColor_lining = makeColorHSB(0, 0, 255);
+      faceColor_cloud = makeColorHSB(160, 0, 255);
+
+    }
     silverLining(faceColor_cloud, faceColor_lining);
   }
 }
@@ -101,9 +279,8 @@ void silverLiningDisplay() {
 
 void silverLining(Color faceColor_Cloud, Color faceColor_Lining) {
 
-  Color fadeColor;
-  byte saturation;
 
+  //setColor(faceColor_Cloud);
   FOREACH_FACE(f) {
     if (!isValueReceivedOnFaceExpired(f)) { //if something there aka if within the cloud
 
@@ -121,24 +298,33 @@ void silverLining(Color faceColor_Cloud, Color faceColor_Lining) {
 
 
       if (setupFadeTimer.isExpired()) {
-        setupFadeFace = (setupFadeFace + random(f)) % f; //choose faces within clouds but sometimes exit
+        setupFadeFace = f;
         backgroundTime = SETUP_RED_INTERVAL + random(SETUP_RED_INTERVAL / 2);
         setupFadeTimer.set(backgroundTime + SETUP_FADE_UP_INTERVAL + random(SETUP_FADE_DELAY));
       }
-
+      Color fadeColor;
+      byte saturation;
+      byte hue;
 
       if (setupFadeTimer.getRemaining() < backgroundTime + SETUP_FADE_UP_INTERVAL) {//we are inside the animation
         if (setupFadeTimer.getRemaining() < SETUP_FADE_UP_INTERVAL) {//we are fading from white
           saturation = 255 - map(setupFadeTimer.getRemaining(), 0, SETUP_FADE_UP_INTERVAL, 0, 255);
-          fadeColor = makeColorHSB(sunSpot_hue, saturation, 255);  //sun spots
-        } else {//we are clouds
-          fadeColor = makeColorHSB(0, 0, bri);
+          fadeColor = makeColorHSB(160, saturation, bri);
+        } else {
+          sunSpotFade.set(SUN_SPOT_FADE);
+          if (!sunSpotFade.isExpired()) {
+            saturation =  map(sunSpotFade.getRemaining(), SUN_SPOT_FADE, 0, 0, 255);
+
+            fadeColor = makeColorHSB(sunSpot_hue, saturation, bri);
+          }
+
         }
 
         setColorOnFace(fadeColor, setupFadeFace);
       }
-      Color faceColor = makeColorHSB(160, 150, bri);
-      setColorOnFace(faceColor, f);
+
+      fadeColor = makeColorHSB(160, 255, bri);
+      setColorOnFace(fadeColor, f);
     }
 
 
@@ -149,6 +335,10 @@ void silverLining(Color faceColor_Cloud, Color faceColor_Lining) {
 
   }
 }
-byte getMode(byte data) {
-  return (data >> 5);               //the value in the first bit
+byte getGameMode(byte data) {
+  return (data & 3);//returns bits E and F
+}
+
+byte getSignalState(byte data) {
+  return ((data >> 2) & 3);//returns bits C and D
 }
